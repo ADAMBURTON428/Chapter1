4159cc7e3a912ad0f6b066926455ad9c30b39cfc
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-08-27 10:05:22 -0500

    update

---
+++ Exercises/src/chapter1_excercies/MovieQuote.java
@@ -0,0 +1,15 @@
+package chapter1_excercies;
+
+public class MovieQuote {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("You must come back with");
+		System.out.println("Back where");
+		System.out.println("Back to the future");
+		
+		
+	}
+
+}
--- Exercises/src/chapter1_excercies/TablesAndChairs.java
+++ Exercises/src/chapter1_excercies/TablesAndChairs.java
@@ -2,4 +2,33 @@ package chapter1_excercies;
 
 public class TablesAndChairs {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+		
+		
+		System.out.println("X                                X");
+		System.out.println("X                                X");
+		System.out.println("X                                X");
+		System.out.println("X                                X");
+		System.out.println("X           XXXXXXXXXXXXX        X");
+		System.out.println("X           X           X        X");
+		System.out.println("XXXXXX      X           X   XXXXXX");
+		System.out.println("X    X      X           X   X    X");
+		
+		displayTruck();
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXX               ");
+		System.out.println("                      	 X              ");
+		System.out.println("                         X            ");
+		System.out.println("                         X          ");
+		System.out.println("                         XXXXXXXXX");
+		System.out.println("                                 l");
+		System.out.println("                                 l");
+		System.out.println("                                 l");
+		System.out.println("XXXXXOXXXXXXXXXXXXXXXXXXXXXXOXXXXX");
+	}
+	
+		public static void displayTruck() {
+	
+}
 }
