5d8d750d024635e239afcf67ccb10443262e0d84
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-08-28 09:37:14 -0500

    Updated File

--- Exercises/src/Sammys_Seashore_Supplies/SammysMotto2.java
+++ Exercises/src/Sammys_Seashore_Supplies/SammysMotto2.java
@@ -4,7 +4,13 @@ public class SammysMotto2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+		System.out.println("sSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSs");
+		System.out.println("sSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSs");
+		System.out.println("s  Sammy's makes it fun in the sun  s");
+		System.out.println("sSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSs");
+		System.out.println("sSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSs");
+		System.out.println("sSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSs");
 	}
 
 }
