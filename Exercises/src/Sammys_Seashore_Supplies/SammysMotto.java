a9c6df40870c8ce05411d20cd0391d31d2c452d7
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-08-28 09:24:16 -0500

    Updated Files

---
+++ Exercises/src/Sammys_Seashore_Supplies/SammysMotto.java
@@ -0,0 +1,10 @@
+package Sammys_Seashore_Supplies;
+
+public class SammysMotto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Exercises/src/Sammys_Seashore_Supplies/SammysMotto2.java
@@ -0,0 +1,10 @@
+package Sammys_Seashore_Supplies;
+
+public class SammysMotto2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
