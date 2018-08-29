7a1e59ab1ac7356ebde90dec5e4869630fbe89af
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-08-29 09:43:06 -0500

    updated files

---
+++ Chapter 2/src/YouDolt/DataDemo.java
@@ -0,0 +1,16 @@
+package YouDolt;
+
+public class DataDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int aWholeNumber = 315;
+		final int SATES_IN_US = 50;
+		System.out.print("The number is ");
+		System.out.print(aWholeNumber);
+		System.out.print("The number of states is " + SATES_IN_US);
+
+
+	}
+
+}
