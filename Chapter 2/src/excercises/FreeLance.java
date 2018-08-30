91e17e0b1802e590088d064fb176d3e40cd93c7e
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-08-30 09:54:56 -0500

    Updated File

--- Chapter 2/src/excercises/FreeLance.java
+++ Chapter 2/src/excercises/FreeLance.java
@@ -1,10 +1,30 @@
 package excercises;
 
+import java.util.Scanner;
+
 public class FreeLance {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		String name;
+		int num1;
+		int num2;
+		int total;
+		Scanner inputDevice = new Scanner(System.in);
+		
+		System.out.println("What is your name:  ");
+		name = inputDevice.next();
+		
+		System.out.println("Enter a number:  ");
+		num1 = inputDevice.nextInt();
+		
+		System.out.println("Enter another number:  ");
+		num2 = inputDevice.nextInt(); 
+		total = num1 + num2;
+		
+		Syste
+		
 	}
 
 }
