523d44d9a85ba478e855d38131ea97a2b28abc18
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-09-05 09:48:37 -0500

    updated

---
+++ Chapter 2/src/excercises/NauticalMiles.java
@@ -0,0 +1,21 @@
+package excercises;
+
+public class NauticalMiles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double kilometer = 1.852;
+		double kilometer2;
+		double miles = 1.150779;
+		double miles2;
+		double nauticalMiles;
+		
+		kilomenter2 = kilo 
+		
+		
+		
+		
+	}
+
+}
