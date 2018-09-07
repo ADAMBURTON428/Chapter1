bd1a25baf983adb8a24e8edf9cf3fc0b5f1a6a63
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-09-07 09:21:45 -0500

    updated file

--- Chapter 2/src/excercises/Eggs.java
+++ Chapter 2/src/excercises/Eggs.java
@@ -1,10 +1,59 @@
 package excercises;
 
+import java.util.Scanner;
+
 public class Eggs {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		double dozenCost = 3.25;
+		int dozen = 12 ;
+		int eggsNeeded;
+		int numberofCarton;
+		int eggRemainging;
+		double eggCost = 0.45;
+		int eggTotal;
+		double eggTotalCost;
+		Scanner input = new Scanner(System.in);
+		
+		
+		System.out.print("Enter the amount eggs you need");
+		eggsNeeded = input.nextInt();
+		
+		if (eggsNeeded >= 12) {
+			numberofCarton = eggsNeeded / dozen;
+			eggRemainging = eggsNeeded % dozen;
+			eggTotalCost = numberofCarton * dozenCost + eggRemainging * eggCost;
+			System.out.print("That will be $ " + eggTotalCost + " and " + numberofCarton 
+					+ " Carton and " + eggRemainging + " spare eggs.");
+			
+		}
+		else {
+			
+			eggTotalCost = eggsNeeded * eggCost;
+			
+			System.out.print("That will be $ " + eggTotalCost + " for " + eggsNeeded + " eggs.");
+			
+			}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
