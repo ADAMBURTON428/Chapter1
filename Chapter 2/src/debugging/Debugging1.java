ea24a8c7e6b3df6f37cb6b316fbe6200d49fa89a
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-09-10 09:10:50 -0500

    update finshed work

--- Chapter 2/src/debugging/Debugging1.java
+++ Chapter 2/src/debugging/Debugging1.java
@@ -4,7 +4,14 @@ public class Debugging1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int oneInt = 315;
+	    double oneDouble = 12.4;
+	    char oneChar = 'A';
+	    System.out.println("The int is " + oneInt);
+	    System.out.println("The double is " + oneDouble);
+	    System.out.println("The char is " + oneChar);
+	   
 	}
 
 }
--- Chapter 2/src/debugging/Debugging2.java
+++ Chapter 2/src/debugging/Debugging2.java
@@ -5,6 +5,11 @@ public class Debugging2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	     int a = 7;
+	     int b = 4;
+	      System.out.println("The sum is " + (a + b));
+	      System.out.println("The difference is " + (a - b));
+	      System.out.println("The product is " + (a * b));
 	}
 
 }
