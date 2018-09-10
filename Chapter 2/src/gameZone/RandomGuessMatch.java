749c9d77b9a67d7996a3392062e426e86b64da1a
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-09-10 09:17:12 -0500

    updated

--- Chapter 2/src/gameZone/RandomGuessMatch.java
+++ Chapter 2/src/gameZone/RandomGuessMatch.java
@@ -5,6 +5,12 @@ public class RandomGuessMatch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int guess;
+		int random;
+		
+		random = 1 + (int)(Math.random() * MAX;
+		
+		
 	}
 
 }
